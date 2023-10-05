f86a8a00d2bf72c2724c53d758f83871a2c501b1
Mohammadhossein Malekpour <mhmalekpour@Sansfil-Eduroam-Etudiants-58-165.polymtl.ca>
2023-10-05 14:26:35 -0400

    add get_vpc and get_subnet and ensure VPC and Subnet with the specified CIDR block exists. If it doesn't, create it.

--- ec2_utils.py
+++ ec2_utils.py
@@ -1,7 +1,25 @@
-def get_vpc(ec2):
-    response = ec2.describe_vpcs()
-    vpc_id = response.get('Vpcs', [{}])[0].get('VpcId', '')
-    return vpc_id    
+def get_vpc(ec2, cidr_block):
+    # Ensure a VPC with the specified CIDR block exists. If it doesn't, create it.
+    vpcs = ec2.describe_vpcs(Filters=[{'Name': 'cidr', 'Values': [cidr_block]}])
+
+    if vpcs and vpcs.get('Vpcs'):
+        return vpcs['Vpcs'][0]['VpcId']
+    else:
+        vpc = ec2.create_vpc(CidrBlock=cidr_block)
+        return vpc['Vpc']['VpcId']
+
+
+def get_subnet(ec2, vpc_id, cidr_block, availability_zone):
+    # Ensure a subnet with the specified CIDR block within the given VPC exists. If it doesn't, create it.
+    subnets = ec2.describe_subnets(
+        Filters=[{'Name': 'cidr-block', 'Values': [cidr_block]}, {'Name': 'vpc-id', 'Values': [vpc_id]}])
+
+    if subnets and subnets.get('Subnets'):
+        return subnets['Subnets'][0]['SubnetId']
+    else:
+        subnet_response = ec2.create_subnet(VpcId=vpc_id, CidrBlock=cidr_block, AvailabilityZone=availability_zone)
+        return subnet_response['Subnet']['SubnetId']
+
 
 def create_key_pair(ec2, key_name):
     with open('my_keypair.pem', 'w') as file:
@@ -10,45 +28,48 @@ def create_key_pair(ec2, key_name):
     key_pair_id = key_pair.get('KeyPairId')
     return key_pair_id
 
+
 def create_security_group(ec2, vpc_id, group_name):
     response = ec2.create_security_group(
-            GroupName=group_name,
-            Description='',
-            VpcId=vpc_id
-        )
+        GroupName=group_name,
+        Description='tmp',
+        VpcId=vpc_id
+    )
     security_group_id = response['GroupId']
     return security_group_id
-    
+
+
 def set_security_group_rules(ec2, sec_group_id):
     ec2.authorize_security_group_ingress(
-                GroupId=sec_group_id,
-                IpPermissions=[
-                    {'IpProtocol': 'tcp', 
-                    'FromPort': 80,
-                    'ToPort': 80,
-                    'IpRanges': [{'CidrIp': '0.0.0.0/0'}]
-                    },
-                ]
-            )
+        GroupId=sec_group_id,
+        IpPermissions=[
+            {'IpProtocol': 'tcp',
+             'FromPort': 80,
+             'ToPort': 80,
+             'IpRanges': [{'CidrIp': '0.0.0.0/0'}]
+             },
+        ]
+    )
+
 
 def lunch_ec2_instance(ec2, image_id, instance_type, key_name, sec_group, zone, profile, tags):
     response = ec2.run_instances(
-            ImageId=image_id,
-            MinCount=1,
-            MaxCount=1,
-            InstanceType=instance_type,
-            KeyName=key_name,
-            SecurityGroups=[sec_group],
-            Placement={
-                'AvailabilityZone': zone
-            },
-            TagSpecifications=tags,
-            IamInstanceProfile={
-                'Name': profile
-            },
-            MetadataOptions={
-                'InstanceMetadataTags': 'enabled'
-            }
-        )
+        ImageId=image_id,
+        MinCount=1,
+        MaxCount=1,
+        InstanceType=instance_type,
+        KeyName=key_name,
+        SecurityGroups=[sec_group],
+        Placement={
+            'AvailabilityZone': zone
+        },
+        TagSpecifications=tags,
+        IamInstanceProfile={
+            'Name': profile
+        },
+        MetadataOptions={
+            'InstanceMetadataTags': 'enabled'
+        }
+    )
     ec2_instance_id = response['Instances'][0]['InstanceId']
     return ec2_instance_id
