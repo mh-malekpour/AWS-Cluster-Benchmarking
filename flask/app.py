4b67b6a21b9427a11b681e0db9d83dffb4f12d18
Mohammadhossein Malekpour <mohammadhossein.malekpour@gmail.com>
2023-10-11 14:23:32 -0400

    implement flask app and bash script to running that

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
+import requests
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def get_instance_id():
+    # Get instance metadata
+    response = requests.get('http://169.254.169.254/latest/meta-data/instance-id')
+    instance_id = response.text
+    return jsonify(instance_id=instance_id)
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
